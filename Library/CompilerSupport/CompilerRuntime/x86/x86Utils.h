b466db69e2cf895fda9a0bb1163285c3b7fdaae9
Oever González <notengobattery@gmail.com>
2020-02-02 22:47:31 -0600

    [git] Update gitignore
    Signed-off-by: Oever González <notengobattery@gmail.com>

---
+++ Library/CompilerSupport/CompilerRuntime/x86/x86Utils.h
@@ -0,0 +1,26 @@
+//===-- x86Utils.c - x86 Specific Utilities for CompilerRuntime -------------------------------------------*- C -*-===//
+//
+// Copyright (c) 2020 Oever González
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+//  the License. You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+//  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+//  specific language governing permissions and limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===--------------------------------------------------------------------------------------------------------------===//
+///
+/// \file
+/// This file cotains macros, variables, definitions, alias, typedefs and other structures needed to compile the
+/// CompilerRuntime runtime, which are specific to x86.
+///
+//===--------------------------------------------------------------------------------------------------------------===//
+#ifndef ZALEA_X_86_UTILS_H
+#define ZALEA_X_86_UTILS_H
+
+#endif //ZALEA_X_86_UTILS_H
