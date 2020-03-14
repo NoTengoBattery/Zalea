3f97e49cb22b2177b69777dc755475a92c720572
Oever González <notengobattery@gmail.com>
2020-03-14 14:22:00 -0600

    [All] Update FANCY_LIST feature disables
    Generally speaking, LTO helps a lot by unintentionally resolving the
    "dependencies", so you don't really need to disable the fancy features
    in all of the libraries, just on the files that are meant to be built
    that way. That is because the optimizer will recognize the flags during
    linking and emit code with the updated flags (overriding the previous
    flags).
    
    Signed-off-by: Oever González <notengobattery@gmail.com>

--- Scripts/CMake/Modules/Platform/Kernel-GNU-CXX.cmake
+++ Scripts/CMake/Modules/Platform/Kernel-GNU-CXX.cmake
@@ -56,7 +56,6 @@ IF (TREE_SELF_PATH) # This will define if we have access to the scope variables 
   STRING(APPEND CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-O2 -g -DRELWITHDEBINFO ")
 
   # These flags will disable any fancy features (needed to build very early boot code or the compiler runtime)
-  SET(CXX_NO_LTO "-fno-lto")
   SET(CXX_NO_SANITIZER "-fno-sanitize=all")
   SET(CXX_NO_RTTI "-fno-rtti")
   SET(CXX_NO_UNWIND "-fno-unwind-tables")
